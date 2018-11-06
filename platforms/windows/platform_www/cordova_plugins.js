7392d50a6e9e9be3982f9efc6a992552d3aac592
Kien <kiennt@zotasoft.com>
2018-11-06 10:05:42 +0700

    vuejs

---
+++ platforms/windows/platform_www/cordova_plugins.js
@@ -0,0 +1,42 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+  {
+    "id": "cordova-plugin-console.logger",
+    "file": "plugins/cordova-plugin-console/www/logger.js",
+    "pluginId": "cordova-plugin-console",
+    "clobbers": [
+      "cordova.logger"
+    ]
+  },
+  {
+    "id": "cordova-plugin-console.console",
+    "file": "plugins/cordova-plugin-console/www/console-via-logger.js",
+    "pluginId": "cordova-plugin-console",
+    "clobbers": [
+      "console"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.statusbar",
+    "file": "plugins/cordova-plugin-statusbar/www/statusbar.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "clobbers": [
+      "window.StatusBar"
+    ]
+  },
+  {
+    "id": "cordova-plugin-statusbar.StatusBarProxy",
+    "file": "plugins/cordova-plugin-statusbar/src/windows/StatusBarProxy.js",
+    "pluginId": "cordova-plugin-statusbar",
+    "runs": true
+  }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+  "cordova-plugin-console": "1.0.7",
+  "cordova-plugin-statusbar": "1.0.1",
+  "cordova-plugin-whitelist": "1.2.2"
+};
+// BOTTOM OF METADATA
+});
